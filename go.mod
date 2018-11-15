bc818815427bd014f81aba10aa6caf1656415494
nicholasdaniel <nicholasjdaniel@gmail.com>
2018-11-14 20:34:22 -0500

    Add go mod and hash

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/njdaniel/monitor
+
+require (
+	github.com/beorn7/perks v0.0.0-20180321164747-3a771d992973 // indirect
+	github.com/golang/protobuf v1.2.0 // indirect
+	github.com/gorilla/mux v1.6.2
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/prometheus/client_golang v0.9.1
+	github.com/prometheus/client_model v0.0.0-20180712105110-5c3871d89910 // indirect
+	github.com/prometheus/common v0.0.0-20181113130724-41aa239b4cce // indirect
+	github.com/prometheus/procfs v0.0.0-20181005140218-185b4288413d // indirect
+)
--- server.go
+++ server.go
@@ -1,25 +1,56 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	histogram := prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name: "hash_seconds",
+		Help: "Time taken to create hashes",
+	}, []string{"code"}
+
 	r := mux.NewRouter()
 
 	r.Handle("/metrics", prometheusHandler())
-	r.Handle("/hash", hashHandler())
+	r.Handle("/hash", hashHandler(histogram))
 
 	log.Fatal(http.ListenAndServe(":8002", r))
 }
 
 func prometheusHandler() http.Handler {
-	return prometheus.Handler()
+	return promhttp.Handler()
 }
 
-func hashHandler() {
+func hashHandler(histogram *prometheus.HistogramVec) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		defer r.Body.Close()
+		code := http.StatusInternalServerError
 
+		defer func() {
+			duration := time.Since(start)
+			histogram.WithLabelValues(fmt.Sprintf("%d", code)).Observe(duration.Seconds())
+		}()
+
+		//code = http.StatusOK
+
+	}
+}
+
+func computeSum(body []byte) []byte {
+	h := sha256.New()
+
+	h.Write(body)
+	hashed := hex.EncodeToString(h.Sum(nil))
+	return []byte(hashed)
 }
